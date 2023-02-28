cb82ff2beca36dcfa9512d613f23c7fe0c18f587
Mehmet Nadi <mhmtalperennadi@gmail.com>
2023-02-28 14:56:18 -0800

    make msg post request to db

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // importing
 import express from "express";
 import mongoose from "mongoose";
-
+import Messages from "./dbMessages.js";
 //app config
 const app = express();
 const port = process.env.PORT || 9000;
@@ -18,6 +18,16 @@ mongoose.connect(mongoodbConnectionURL);
 
 // api routes
 app.get("/", (req, res) => res.status(200).send("fart"));
+
+app.post("/api/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage)
+    .then((data) => {
+      res.status(201).send(data);
+    })
+    .catch((err) => res.status(500).send(err));
+});
 
 // listeners
 app.listen(port, () => console.log(`Listeningfdf sfs on localhost: ${port}`));
